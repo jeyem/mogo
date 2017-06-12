6846825293d9051920b935b69a1313b97aa258d3
Ehsan Mahmoudi <me@e-mahmoudi.me>
2017-06-12 11:58:27 +0430

    add pagination

--- query.go
+++ query.go
@@ -3,6 +3,7 @@ package mogo
 import (
 	"reflect"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -12,6 +13,10 @@ type Query struct {
 	selectCulomn      bson.M
 	selectedOneColumn bool
 	sort              string
+	querySet          *mgo.Query
+	paginated         bool
+	skip              int
+	limit             int
 }
 
 func (q *Query) Select(s bson.M) *Query {
@@ -30,30 +35,61 @@ func (q *Query) Sort(s string) *Query {
 	return q
 }
 
+func (q *Query) Paginate(limit, page int) *Query {
+	q.paginated = true
+	q.limit = limit
+	q.skip = page * limit
+	return q
+}
+
+func (q *Query) Limit(limit int) *Query {
+	q.paginated = true
+	q.limit = limit
+	q.skip = 0
+	return q
+}
+
 func (q *Query) Find(model interface{}) error {
-	if len(q.q) < 2 {
-		err := q.result(model, q.q[0])
-		return err
-	}
-	query := bson.M{"$or": q.q}
-	return q.result(model, query)
+	query := q.parseQuery()
+	q.loadQuerySet(model, query)
+	return q.result(model)
+}
+
+func (q *Query) Count(model interface{}) (int, error) {
+	query := q.parseQuery()
+	q.loadQuerySet(model, query)
+	return q.querySet.Count()
 }
 
 // ----------------------- in package -------------------
 
-func (q *Query) result(model interface{}, query bson.M) error {
+func (q *Query) parseQuery() bson.M {
+	if len(q.q) < 2 {
+		return q.q[0]
+	}
+	return bson.M{"$or": q.q}
+}
+
+func (q *Query) loadQuerySet(model interface{}, query bson.M) {
 	col := q.db.Collection(model)
 	mgoQuery := col.Find(query)
 	if q.selectedOneColumn {
 		mgoQuery = mgoQuery.Select(q.selectCulomn)
 	}
+	if q.paginated {
+		mgoQuery.Skip(q.skip).Limit(q.limit)
+	}
 	if q.sort != "" {
 		mgoQuery = mgoQuery.Sort(q.sort)
 	}
+	q.querySet = mgoQuery
+}
+
+func (q *Query) result(model interface{}) error {
 	if isSlice(model) {
-		return mgoQuery.All(model)
+		return q.querySet.All(model)
 	}
-	return mgoQuery.One(model)
+	return q.querySet.One(model)
 }
 
 func isSlice(model interface{}) bool {
